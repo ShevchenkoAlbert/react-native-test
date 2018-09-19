e9c744a4b74c95294e0e9cdacfd34f603b6d86d5
Albert Shevchenko <shevchenkoalbert@shevchenkoalbert-lx.nixsolutions.com>
2018-09-19 16:10:23 +0300

    fix icons on android

--- android/app/src/main/java/com/course/MainApplication.java
+++ android/app/src/main/java/com/course/MainApplication.java
@@ -26,6 +26,7 @@ public class MainApplication extends NavigationApplication {
         // No need to add RnnPackage and MainReactPackage
         return Arrays.<ReactPackage>asList(
             // eg. new VectorIconsPackage()
+            new VectorIconsPackage()
         );
     }
 
