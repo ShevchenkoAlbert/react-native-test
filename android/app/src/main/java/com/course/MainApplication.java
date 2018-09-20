1f18498d53f5405d6eb08a069961ec62dcdbcc66
Albert Shevchenko <shevchenkoalbert@shevchenkoalbert-lx.nixsolutions.com>
2018-09-20 19:00:46 +0300

    add firebase

--- android/app/src/main/java/com/course/MainApplication.java
+++ android/app/src/main/java/com/course/MainApplication.java
@@ -3,12 +3,15 @@ package com.course;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+// import com.reactnativenavigation.NavigationReactPackage;
+import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.reactnativenavigation.NavigationApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,13 +29,20 @@ public class MainApplication extends NavigationApplication {
         // No need to add RnnPackage and MainReactPackage
         return Arrays.<ReactPackage>asList(
             // eg. new VectorIconsPackage()
-            new VectorIconsPackage()
+            new VectorIconsPackage(),
+            new ImagePickerPackage(),
+            new MapsPackage()
         );
     }
 
     @Override
     public List<ReactPackage> createAdditionalReactPackages() {
         return getPackages();
+    }
+
+    @Override
+        public String getJSMainModuleName() {
+    return "index";
     }
 }
 
--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,9 +1,20 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
-export const addPlace = (placeName) => {
-    return {
-        type: ADD_PLACE,
-        placeName: placeName
+export const addPlace = (placeName, location, image) => {
+    return  dispatch => {
+        const placeData = {
+            name: placeName,
+            location: location
+        };
+        fetch("https://course-5f255.firebaseio.com/places.json", {
+            method: "POST",
+            body: JSON.stringify(placeData)
+        })
+        .catch(err => console.log(err))
+        .then(res => res.json())
+        .then(parsedRes => {
+            console.log(parsedRes);
+        });
     };
 };
 
